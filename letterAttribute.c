ccec86c0018a252a6a48b632db95f3e4dad4859e
CTreeB <119283736+CTreeB@users.noreply.github.com>
2023-11-17 11:15:31 +0900

    Update letterAttribute.c

--- letterAttribute.c
+++ letterAttribute.c
@@ -5,18 +5,18 @@
 #define SHADOW (0x01 << 2)
 #define UL     (0x01 << 3)
 
-//괄호를 묶어서 연산자 우선순위를 정해줘야, 정확한 출력을 할 수 있다.
-//0x02 << 1,0x04 << 2, 0x08 << 3
-
 int main(void)
 
 {
-	unsigned char attr;
+	unsigned char attr, attr1, attr2, attr3;
 	attr = attr ^ attr;
-	attr = attr | BOLD;
-	attr = attr | SHADOW + ITALIC;
-	attr = attr & (~BOLD);
-	
+	attr1 = attr | BOLD;
+	attr2 = attr | SHADOW + ITALIC;
+	attr3 = attr & (~BOLD);
+
 	printf("attr: 0x%02x\n", attr);
+	printf("attr: 0x%02x\n", attr1);
+	printf("attr: 0x%02x\n", attr2);
+	printf("attr: 0x%02x\n", attr3);
 	return 0;
 }
