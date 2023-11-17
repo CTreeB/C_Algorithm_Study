f98e8e5b56c46f7b7a6e1128a5c60f6fe570825c
CTreeB <119283736+CTreeB@users.noreply.github.com>
2023-11-17 11:12:35 +0900

    Update leapYear3.c

--- leapYear3.c
+++ leapYear3.c
@@ -4,20 +4,16 @@ int main(void)
 {
 	int year;
 	printf("input year : ");
-	scanf("%d", &year);
-	
-	//int isLeap = (( year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0);
-//int isLeap = (( year % 400 == 0 || year % 4 == 0 && year % 100 != 0); -> 400으로 나누면 확률이 떨어진다.
-/*
+	std::cin >> year;
+
+
 	if((year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0){
 	printf("%d is a leap\n", year);
 	}else {
 	printf("%d is a ordinary\n", year);
 	}
-*/	
-	//printf("%d is a leap : %d\n", year, isLeap);
 
-	printf("%d is a %s year\n", year,((year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0) ? "leap" : "ordinary");
-	
+	printf("%d is a %s year\n", year, ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) ? "leap" : "ordinary");
+
 	return 0;
 }
