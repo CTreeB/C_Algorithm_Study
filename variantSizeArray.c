a8ff8da69db58b0411c375db677a4f65c3b1248c
CTreeB <119283736+CTreeB@users.noreply.github.com>
2023-11-17 11:22:42 +0900

    Update variantSizeArray.c

--- variantSizeArray.c
+++ variantSizeArray.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
+#include<vector>
 
 int main(void)
 {
-	int size;
+	int size = 0;
 	printf("input size: ");
-	scanf("%d", &size);
-	
-	int array[size]; 
-	
+	std::cin >> size;
+	std::vector<int> array(size);
+
 	for (int i = 0; i < size; ++i) {
 		array[i] = i + 1;
 	}
-	
+
 	for (int i = 0; i < size; ++i) {
 		printf("%d ", array[i]);
 	}
+
 	printf("\n");
-	
+
 	return 0;
 }
